79f173dd88e840d82ec402f5f15bd3c0e8af4a7a
Isbat-ulIslam <isbatislam@gmail.com>
2025-02-18 23:29:49 -0500

    LAB2 Vref+Temp measurement

---
+++ Lab 2/Core/Src/measurement.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdint.h>
+
+#define VREFINT_ADDR  ((uint16_t*)0x1FFF75AA)
+#define TS_CAL1_ADDR      ((uint16_t*)0x1FFF75A8) // 30°C
+#define TS_CAL2_ADDR      ((uint16_t*)0x1FFF75CA) // 130°C
+
+float getVoltage(ADC_HandleTypeDef *hadc) {
+    uint32_t adc_value;
+    uint16_t vrefint_cal = *VREFINT_ADDR;
+
+    if (HAL_ADCEx_Calibration_Start(hadc, ADC_SINGLE_ENDED) != HAL_OK) {
+      Error_Handler();
+    }
+
+    HAL_ADC_Start(hadc);
+    HAL_Delay(1);
+    HAL_ADC_PollForConversion(hadc, HAL_MAX_DELAY);
+    adc_value = HAL_ADC_GetValue(hadc);
+    HAL_ADC_Stop(hadc);
+
+    float vRefPlus = (3.0f * vrefint_cal) / adc_value;
+
+    return vRefPlus;
+}
+
+float getTemperature(ADC_HandleTypeDef *hadc) {
+    uint32_t adc_value;
+    uint16_t ts_cal1 = *TS_CAL1_ADDR;  // 30°C
+    uint16_t ts_cal2 = *TS_CAL2_ADDR;  // 130°C
+
+    // Read value from injected channel (which is responsible for TEMP)
+    HAL_ADCEx_InjectedStart(hadc);
+    HAL_Delay(1);
+    adc_value = HAL_ADCEx_InjectedGetValue(hadc, ADC_INJECTED_RANK_1);
+
+    float vRef = getVoltage(hadc);  // Read actual VREF+
+    float adj_adc_value = adc_value * (vRef / 3.0f);
+    float temperature = ((100.0f / (float)(ts_cal2 - ts_cal1)) *
+                         (adj_adc_value - ts_cal1)) + 30.0f;
+    return temperature;
+}
