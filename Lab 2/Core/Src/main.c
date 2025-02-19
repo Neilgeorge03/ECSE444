000399a138986bd8ca29ac57df6376b6e8bd4e83
Isbat-ulIslam <isbatislam@gmail.com>
2025-02-19 16:21:06 -0500

    Working everything. Need to add LED

--- Lab 2/Core/Src/main.c
+++ Lab 2/Core/Src/main.c
@@ -244,7 +244,7 @@ static void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
-  sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
+  sConfig.Channel = ADC_CHANNEL_VREFINT;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_247CYCLES_5;
   sConfig.SingleDiff = ADC_SINGLE_ENDED;
