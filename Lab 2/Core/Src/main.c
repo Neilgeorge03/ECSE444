b4df449e74c89280e09b58c2ac200800b60a3775
Neilgeorge03 <49652125+Neilgeorge03@users.noreply.github.com>
2025-02-19 01:30:01 -0500

    UART Trial

--- Lab 2/Core/Src/main.c
+++ Lab 2/Core/Src/main.c
@@ -44,6 +44,8 @@
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
 
+UART_HandleTypeDef huart1;
+
 /* USER CODE BEGIN PV */
 volatile uint8_t state;
 #define VREF_CAL_ADDR ((uint16_t*)0x1FFF75AA)
@@ -58,6 +60,7 @@ volatile uint8_t state;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_ADC1_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -118,6 +121,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_ADC1_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   int8_t data[100];
   int8_t uartLen;
@@ -132,17 +136,21 @@ int main(void)
   		  case 0:
   			  voltage = getVoltage();
   			  uartLen = sprintf(data, "The voltage is: %.2fV\n", voltage);
-  			  for (int i = 0; i < uartLen; i++){
-  				ITM_SendChar(data[i]);
-  			  }
-  			  break;
+  			  HAL_UART_Transmit(&huart1, data, uartLen, 1000);
+
+//  			  for (int i = 0; i < uartLen; i++){
+//  				ITM_SendChar(data[i]);
+//  			  }
+//  			  break;
 
   		  case 1:
   			  temperature = getTemperature();
   			  uartLen = sprintf(data, "The temperature is: %.2f%cC\n", temperature, 176);
-  			  for (int i = 0; i < uartLen; i++){
-  				ITM_SendChar(data[i]);
-  			  }
+  			  HAL_UART_Transmit(&huart1, data, uartLen, 1000);
+
+//  			  for (int i = 0; i < uartLen; i++){
+//  				ITM_SendChar(data[i]);
+//  			  }
   			  break;
   		  default:
   			  uartLen = sprintf("The state is: %d\n", state);
@@ -294,6 +302,54 @@ static void MX_ADC1_Init(void)
 }
 
 /**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -307,6 +363,7 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
